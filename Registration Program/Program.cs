993895ebbeb939bb08384b9ddbe55407d96cb18e
Mazen Kamel <mazenwkamel@gmail.com>
2022-01-17 03:51:06 +0200

    Initial Commit

---
+++ Registration Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Registration_Program
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
